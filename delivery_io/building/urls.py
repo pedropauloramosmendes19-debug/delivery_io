5579d3fc548804b64eef4df9999676f45596390f
pedropauloramosmendes19-debug <pedro.pauloramosmendes19@gmail.com>
2025-12-25 19:24:04 -0300

    Fix: remove leading slash from buildings API route

--- delivery_io/building/urls.py
+++ delivery_io/building/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BuildingListAPIView
 
 urlpatterns = [
-    path('/api/buildings/', BuildingListAPIView.as_view(), name='api_buildings_list'),
+    path('api/buildings/', BuildingListAPIView.as_view(), name='api_buildings_list'),
 ]
